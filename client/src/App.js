68d08cd2d97ea44091f7a18af3046d2775340630
vladmog <vladmog@gmail.com>
2019-07-19 08:29:01 -0700

    Implemented Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './LoginForm';
 
 function App() {
   return (
     <div className="App">
       <h1>App.js</h1>
+      <LoginForm />
     </div>
   );
 }
